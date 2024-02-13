52db86a1650f2edfdeea9ebb2692b5b504c7b40c
Hai Tran <haitn2806@gmail.com>
2024-02-13 16:26:19 +0700

    update

---
+++ public/routes/products.routes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const products_controllers_1 = require("../controllers/products.controllers");
+const productsRoutes = (0, express_1.Router)();
+productsRoutes.get('/', products_controllers_1.getAllProducts);
+productsRoutes.get('/:productId', products_controllers_1.getProductById);
+productsRoutes.post('/', products_controllers_1.addProducts);
+productsRoutes.delete('/:productId', products_controllers_1.deleteProducts);
+productsRoutes.delete('/:productId/options/:optionId', products_controllers_1.deleteOptions);
+productsRoutes.post('/:productId/options', products_controllers_1.addProductsVariant);
+exports.default = productsRoutes;
