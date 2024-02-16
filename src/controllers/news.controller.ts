1c26876c0b6d78bfdd20ffc14ff225360a3d550a
Hai Tran <haitn2806@gmail.com>
2024-02-16 13:40:26 +0700

    update

--- src/controllers/news.controller.ts
+++ src/controllers/news.controller.ts
@@ -3,12 +3,12 @@ import { Request, Response, NextFunction, Router } from 'express'
 import mongoose, { Schema, Document } from 'mongoose'
 
 import { ObjectId } from 'mongodb'
-import databaseService from 'src/services/database.services'
+import databaseService from '../services/database.services'
 
 export const addNews = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // Kết nối tới database nếu cần
-    const { name, description, status_news, date_create, date_update} = req.body // Mảng options rỗng
+    const { name, description, status_news, date_create, date_update } = req.body // Mảng options rỗng
 
     // Tạo sản phẩm mới với mảng options rỗng
     const newInsertion = await databaseService.news.create({
@@ -30,37 +30,37 @@ export const addNews = async (req: Request, res: Response, next: NextFunction) =
 }
 
 export const updateNews = async (req: Request, res: Response) => {
-    const { nameId, newId } = req.params
-    const { elementId, newValue } = req.body 
-  
-    // Kiểm tra tính hợp lệ của ID
-    if (!mongoose.Types.ObjectId.isValid(newId) || !mongoose.Types.ObjectId.isValid(elementId)) {
-      return res.status(404).send('Invalid ID')
-    }
-  
-    const validNames = ['name', 'description', 'status_news', 'date_update']
-    if (!validNames.includes(nameId)) {
-      return res.status(400).send('Invalid array name')
-    }
-  
-    try {
-      const updateQuery: any = {}
-      updateQuery[`${nameId}.$.value`] = newValue
-  
-      const updateResult = await databaseService.news.updateOne(
-        { _id: new ObjectId(newId), [`${nameId}.id`]: new ObjectId(elementId) },
-        { $set: updateQuery }
-      )
-  
-      if (updateResult.modifiedCount === 0) {
-        return res.status(404).send('No element found with that id')
-      }
-  
-      res.status(200).send('Updated successfully')
-    } catch (error: any) {
-      res.status(400).json({ message: error.message })
-    }
+  const { nameId, newId } = req.params
+  const { elementId, newValue } = req.body
+
+  // Kiểm tra tính hợp lệ của ID
+  if (!mongoose.Types.ObjectId.isValid(newId) || !mongoose.Types.ObjectId.isValid(elementId)) {
+    return res.status(404).send('Invalid ID')
   }
+
+  const validNames = ['name', 'description', 'status_news', 'date_update']
+  if (!validNames.includes(nameId)) {
+    return res.status(400).send('Invalid array name')
+  }
+
+  try {
+    const updateQuery: any = {}
+    updateQuery[`${nameId}.$.value`] = newValue
+
+    const updateResult = await databaseService.news.updateOne(
+      { _id: new ObjectId(newId), [`${nameId}.id`]: new ObjectId(elementId) },
+      { $set: updateQuery }
+    )
+
+    if (updateResult.modifiedCount === 0) {
+      return res.status(404).send('No element found with that id')
+    }
+
+    res.status(200).send('Updated successfully')
+  } catch (error: any) {
+    res.status(400).json({ message: error.message })
+  }
+}
 
 export const deleteNews = async (req: Request, res: Response, next: NextFunction) => {
   const { newId } = req.params
