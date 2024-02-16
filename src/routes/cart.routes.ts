468d272f85aeeaee73ec22e8c5ae91e4e45ff973
Hai Tran <haitn2806@gmail.com>
2024-02-16 13:31:06 +0700

    update

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { addToCart, updateCart } from 'src/controllers/cart.controllers'
+import { addToCart, updateCart, getCart } from 'src/controllers/cart.controllers'
 
 const cartRoutes = Router()
 
 cartRoutes.post('/add-to-cart', addToCart)
 cartRoutes.post('/update-cart-item', updateCart)
+cartRoutes.get('/cart/:userId', getCart)
 // productsRoutes.delete('/:productId', deleteProducts)
 // productsRoutes.delete('/:productId/options/:optionId', deleteOptions)
 // productsRoutes.post('/:productId/options', addProductsVariant)
