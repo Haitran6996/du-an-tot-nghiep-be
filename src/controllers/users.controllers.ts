35e7b0529264e60151a17bddff7bdd91a2e552b4
thanhdoan1201 <nokageed9@gmail.com>
2024-02-19 17:25:30 +0700

    update routes api users

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -55,9 +55,9 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const getUserById = async (req: Request, res: Response) => {
   const { usersId } = req.params
   try {
-    const user = await databaseService.users.find({ _id: usersId }).select('-mail -password -refreshToken ');
-    res.status(200).json(user)
+    const user = await databaseService.users.findOne({ _id: usersId }).select('-mail -password -refreshToken ');
+    return res.status(200).json(user)
   } catch (error: any) {
-    res.status(500).json({ message: 'Failed to get data Users', error: error.message })
+    return res.status(500).json({ message: 'Failed to get data Users', error: error.message })
   }
 }
