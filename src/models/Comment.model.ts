9f14d5338c8f67976a8c22002d692433ccfbd0c2
thanhdoan1201 <nokageed9@gmail.com>
2024-02-22 08:59:49 +0700

    update api comment

--- src/models/Comment.model.ts
+++ src/models/Comment.model.ts
@@ -1,15 +1,20 @@
 import mongoose, { Schema, Document } from 'mongoose'
 
 export interface IComment extends Document {
-  userId: mongoose.Types.ObjectId
-  productId: mongoose.Types.ObjectId
-  comment : string
+    userId: mongoose.Types.ObjectId
+    productId: mongoose.Types.ObjectId
+    comment: string
 }
 
 const CommentSchema: Schema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'users', required: true }, // Giả sử bạn có một User model
-  productId: { type: Schema.Types.ObjectId, ref: 'product', required: true },
-  comment: {type: String, require:true}
-})
+    userId: { type: Schema.Types.ObjectId, ref: 'users', required: true }, // Giả sử bạn có một User model
+    productId: { type: Schema.Types.ObjectId, ref: 'product', required: true },
+    comment: { type: String, require: true }
+},
+    {
+        timestamps: true,
+    }
+
+)
 
 export default mongoose.model<IComment>('comment', CommentSchema)
