da81594d7e31cd74ce4200bb56133bc6560e4bae
thanhdoan1201 <nokageed9@gmail.com>
2024-02-27 21:08:37 +0700

    update api user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,13 +4,15 @@ import {
   deleteUsers,
   getAllUsers,
   getUserById,
-  paginationUsers
+  paginationUsers,
+  getUsernameById
 } from '../controllers/users.controllers'
 
 const usersRoutes = Router()
 usersRoutes.get('/pagination/:n/:p', paginationUsers)
 usersRoutes.get('/', getAllUsers)
 usersRoutes.get('/one/:_id', getUserById)
+usersRoutes.get('/username/:userId',getUsernameById)
 usersRoutes.post('/', signUp)
 usersRoutes.delete('/:userId', deleteUsers)
 
