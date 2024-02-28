32c072526d857f68c6e4a87de14a8afc4f8044ea
thanhdoan1201 <nokageed9@gmail.com>
2024-02-28 17:55:47 +0700

    update build public

--- public/controllers/auth/auth.controllers.js
+++ public/controllers/auth/auth.controllers.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = exports.getToken = exports.register = void 0;
+exports.login = exports.getToken = exports.createAdmin = exports.register = void 0;
 const md5_1 = __importDefault(require("md5"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const database_services_1 = __importDefault(require("../../services/database.services"));
@@ -45,6 +45,43 @@ const register = async (req, res, next) => {
     }
 };
 exports.register = register;
+const createAdmin = async (req, res) => {
+    try {
+        // Kết nối tới database nếu cần
+        const { username, mail, password, role } = req.body;
+        const hashPassword = (0, md5_1.default)(password);
+        //Check tài khoản có trong db hay không
+        const checkExistUsername = await database_services_1.default.users.findOne({ username: username });
+        if (checkExistUsername) {
+            return res.status(401).json('Tên đăng nhập đã được sử dụng.');
+        }
+        const checkExistMail = await database_services_1.default.users.findOne({ mail: mail });
+        if (checkExistMail) {
+            return res.status(401).json('Mail đã được sử dụng.');
+        }
+        // Tạo tài khoản mới
+        if (role == 0) {
+            const usersInsertion = await database_services_1.default.users.create({
+                username,
+                mail,
+                password: hashPassword,
+                role
+            });
+            res.status(201).json({
+                message: 'Register successfully',
+                userId: usersInsertion._id
+            });
+        }
+        else if (role == 0) {
+            res.status(401).json('Create failed');
+        }
+    }
+    catch (error) {
+        console.error('Error register:', error);
+        res.status(500).json({ message: 'Failed to register', error: error.message });
+    }
+};
+exports.createAdmin = createAdmin;
 const getToken = async (req, res) => {
     try {
         const { username } = req.body;
