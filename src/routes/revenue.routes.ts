4d9725d28ff549fc2995ca2b20138e044904bfcf
Hai Tran <haitn2806@gmail.com>
2024-03-01 23:59:32 +0700

    update

--- src/routes/revenue.routes.ts
+++ src/routes/revenue.routes.ts
@@ -1,11 +1,12 @@
 // variantRouter.ts
 import express from 'express'
-import { getRevenue } from 'src/controllers/revenue.controllers'
+import { getRevenue, getRevenueYear } from 'src/controllers/revenue.controllers'
 
 const revenueRouters = express.Router()
 
 // Route để tạo biến thể mới
 revenueRouters.get('', getRevenue)
+revenueRouters.get('/total-year', getRevenueYear)
 // vnpayRouters.post('/vnpay_return', returnVnpay)
 
 export default revenueRouters
