05ef501adbd2f122118b16417958cd5bc2556aa9
thanhdoan1201 <nokageed9@gmail.com>
2024-03-02 02:39:49 +0700

    fix api

--- src/controllers/category.controllers.ts
+++ src/controllers/category.controllers.ts
@@ -31,50 +31,35 @@ export const paginationCategory = async (req: Request, res: Response, next: Next
 export const addCategory = async (req: Request, res: Response, next: NextFunction) => {
     try {
         // Kết nối tới database nếu cần
-        const { userId, role, name, image } = req.body // Mảng options rỗng
-
-        // check user
-        const checkRoleUser = await databaseService.users.find({ _id: userId, role: role })
-        // check product
-        if (checkRoleUser[0]._id == userId && checkRoleUser[0]._id == 0) {
-            const CategoryInsertion = await databaseService.categorys.create({
-                name,
-                image
-            }
-            )
-            res.status(201).json({
-                message: 'Categorycode created successfully',
-                CategoryId: CategoryInsertion
-            })
+        const { name, image } = req.body // Mảng options rỗng
+        const CategoryInsertion = await databaseService.categorys.create({
+            name,
+            image
         }
-    } catch (error: any) {
+        )
+        res.status(201).json({
+            message: 'Categorycode created successfully',
+            CategoryId: CategoryInsertion
+        })
+    }
+    catch (error: any) {
         console.error('Error create Category:', error)
         res.status(500).json({ message: 'Failed to create Category', error: error.message })
     }
 }
 
 export const updateCategory = async (req: Request, res: Response) => {
+    const { categoryId } = req.params
+    const { name, image } = req.body
     try {
-        const { CategoryId }= req.params
-        const updateData = req.body // Dữ liệu cập nhật được gửi từ client
-
-        // Tìm sản phẩm để cập nhật
-        const Category: any = await databaseService.categorys.findById(CategoryId)
-
-        if (!Category) {
-            return res.status(404).json({ message: 'Categorycode không tồn tại' })
-        }
-
-        // Cập nhật chỉ các thuộc tính được gửi từ client
-        Object.keys(updateData).forEach((key: any) => {
-            Category[key] = updateData[key]
-        })
-
-        // Lưu sản phẩm đã cập nhật
-        await Category.save()
-
-        // Trả về thông tin sản phẩm đã cập nhật
-        res.json({ message: 'Cập nhật Categorycode thành công', Category })
+        const updateResult = await databaseService.categorys.findByIdAndUpdate(
+            { _id: categoryId },
+            {
+                name: name,
+                image: image,
+            }
+        )
+        res.status(200).json({ message: 'Cập nhật Category thành công' })
     } catch (error) {
         console.error('Lỗi khi cập nhật Categorycode:', error)
         res.status(500).json({ message: 'Đã xảy ra lỗi khi cập nhật Categorycode' })
@@ -82,30 +67,35 @@ export const updateCategory = async (req: Request, res: Response) => {
 }
 
 export const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
-    const { CategoryId, userId, role } = req.body
+    const { categoryId } = req.params
 
     try {
-        const checkRoleUser = await databaseService.users.find({ _id: userId })
-
-        if ( Number(role) == checkRoleUser[0].role && Number(role) == 0) {
-            await databaseService.categorys.deleteOne({ _id: new ObjectId(CategoryId) })
-
-            res.status(200).json({
-                message: 'Categorycode deleted successfully'
-            })
-        }
-    } catch (error: any) {
+        await databaseService.categorys.deleteOne({ _id: new ObjectId(categoryId) })
+        res.status(200).json({
+            message: 'Categorycode deleted successfully'
+        })
+    }
+    catch (error: any) {
         console.error('Error deleting Category:', error)
         res.status(500).json({ message: 'Failed to delete Category', error: error.message })
     }
 }
 
 export const getAllCategory = async (req: Request, res: Response) => {
-    const { userId, role } = req.params
     try {
-        const checkAdmin = await databaseService.users.find({ _id: userId, role: role })
-        if (checkAdmin[0].role == 0) {
+        {
             const news = await databaseService.categorys.find({})
+            res.status(200).json(news)
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: 'Failed to get all category', error: error.message })
+    }
+}
+export const getOneCategory = async (req: Request, res: Response) => {
+    try {
+        const { categoryId } = req.params
+        {
+            const news = await databaseService.categorys.find({ _id: categoryId })
             res.status(200).json(news)
         }
     } catch (error: any) {
--- src/routes/gift.routes.ts
+++ src/routes/gift.routes.ts
@@ -4,6 +4,7 @@ import {
   deleteGift,
   updateGift,
   paginationGift,
+  getAllGift,
   getGiftWithCode
 } from '../controllers/gift.controllers'
 
@@ -12,8 +13,9 @@ const NewsRoutes = Router()
 
 NewsRoutes.get('/pagination/:n/:p', paginationGift)
 NewsRoutes.get('/:code', getGiftWithCode)
+NewsRoutes.get('/', getAllGift)
 NewsRoutes.post('/', addGift)
 NewsRoutes.put('/:giftId', updateGift)
-NewsRoutes.delete('/', deleteGift)
+NewsRoutes.delete('/:giftId', deleteGift)
 
 export default NewsRoutes
