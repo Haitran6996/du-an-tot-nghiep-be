b157e6ca1fccd04710b6cd557878a2e418d7da39
Hai Tran <haitn2806@gmail.com>
2024-03-24 21:52:53 +0700

    update

--- src/services/cart.services.ts
+++ src/services/cart.services.ts
@@ -115,7 +115,7 @@ export async function getCartServices(req: Request, res: Response) {
 
     // Lặp qua từng sản phẩm trong giỏ hàng và tính tổng tiền
     cart.items.forEach((item: any) => {
-      const productPrice = item?.product?.price // Giá của sản phẩm
+      const productPrice = item?.product?.optiions[0].price // Giá của sản phẩm
       const quantity = item?.quantity // Số lượng sản phẩm
       totalAmount += productPrice * quantity // Tính tổng tiền cho sản phẩm này
     })
